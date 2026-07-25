9fca1b72368ff80ed8e761421e5fcba62008041d
Thahmil Mahesh <thahmilmaheshkumar@gmail.com>
2026-07-25 22:44:42 +0530

    cors

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,10 @@ app.use(
 
 app.use(
   cors({
-    origin: "https://ecom-frontend-zeta-three.vercel.app",
+    origin: [
+      "https://ecom-frontend-zeta-three.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   }),
