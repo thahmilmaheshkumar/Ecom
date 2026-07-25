10bd12f2241ea350a61aefbe42bb24e2d3422df7
Thahmil Mahesh <thahmilmaheshkumar@gmail.com>
2026-07-25 22:36:51 +0530

    cookie

--- Backend/helpper/cookie.js
+++ Backend/helpper/cookie.js
@@ -2,5 +2,7 @@ export default (res, token) => {
   res.cookie("token", token, {
     httpOnly: true,
     maxAge: process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000, // 7 days
+    secure: true,
+    sameSite: "none",
   });
 };
