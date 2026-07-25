6c7f27c3518e654c2af808f405c14d0dda502255
Thahmil Mahesh <thahmilmaheshkumar@gmail.com>
2026-07-25 22:59:26 +0530

    credentials

--- Frontend/src/redux/user/userslice.js
+++ Frontend/src/redux/user/userslice.js
@@ -39,6 +39,7 @@ export const login = createAsyncThunk(
       const { data } = await axios.post(
         `${import.meta.env.VITE_API_URL}/api/auth/login`,
         user,
+        { withCredentials: true },
       );
       return data;
     } catch (error) {
